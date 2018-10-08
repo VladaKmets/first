d78a2746f29ddf2d612888ae02c2109da9817f98
unknown <vlada.zakomorna@gmail.com>
2018-10-08 20:34:52 +0300

    Initial commit

---
+++ src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
@@ -0,0 +1,9 @@
+package com.hillel.elementary.com.hillel.elementary;
+
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("Hello!");
+        }
+
+        }
+
---
+++ src/main/java/com/hillel/elementary/project.java
@@ -0,0 +1,4 @@
+package com.hillel.elementary;
+
+public class project {
+}
