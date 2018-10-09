4a76cb6e7ff90eb148495423bb855be3e2d48ece
unknown <vlada.zakomorna@gmail.com>
2018-10-09 23:09:45 +0300

    Back to initial commit

--- src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
+++ src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
@@ -3,8 +3,6 @@ package com.hillel.elementary.com.hillel.elementary;
 public class Main {
         public static void main(String[] args) {
                 System.out.println("Hello!");
-                System.out.println("TEST!");
         }
-
         }
 
