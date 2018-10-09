a8342ce491ae3db949967c08e1e37713c8708f3d
unknown <vlada.zakomorna@gmail.com>
2018-10-09 22:45:04 +0300

    Test commit

--- src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
+++ src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
@@ -3,6 +3,7 @@ package com.hillel.elementary.com.hillel.elementary;
 public class Main {
         public static void main(String[] args) {
                 System.out.println("Hello!");
+                System.out.println("TEST!");
         }
 
         }
