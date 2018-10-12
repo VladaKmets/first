f7f1674c4197c3518a108361c8d56cb9fd4ee037
unknown <vlada.zakomorna@gmail.com>
2018-10-12 17:10:25 +0300

    Changed file

--- src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
+++ src/main/java/com/hillel/elementary/com/hillel/elementary/Main.java
@@ -3,7 +3,7 @@ package com.hillel.elementary.com.hillel.elementary;
 public class Main {
         public static void main(String[] args) {
                 System.out.println("Hello!");
-		System.out.println("test comit from James Bond!:)");
+		System.out.println("test commit from Jack Sparrow!:)");
 
         }
         }
